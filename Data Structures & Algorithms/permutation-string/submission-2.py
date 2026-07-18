f7384d36e67705d9e122b09c789639e9d6813c1e
Mohamed Lamhaouri <79314580+lamhaourii@users.noreply.github.com>
2026-07-18 15:22:41 +0100

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.py
@@ -0,0 +1,24 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        mapp=self.getmap(s1)
+        l=0
+        r=len(s1)
+        mapp2=self.getmap(s2[l:r])
+        while r<len(s2)+1:
+            
+            if self.getmap(s2[l:r])==mapp:
+                return True
+            mapp2[ord(s2[l])-ord('a')]-=1
+            mapp2[ord(s2[r-1])-ord('a')]+=1
+            l+=1
+            r+=1
+
+        return False
+
+    def getmap(self, s:str)->list:
+        mapp=[0]*26
+        for c in s:
+            mapp[ord(c)-ord('a')]+=1
+        return mapp
+
+        
